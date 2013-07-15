410f2bf09d17fe42676f1d032fdd9ac812f3dc1d
Hervé Martinet <herve.martinet@blues2.ch>
2013-07-15 11:00:13 +0200

    Update version

--- bss_resident_permit/__openerp__.py
+++ bss_resident_permit/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Resident Permits',
-    'version': '7.0.1.4',
+    'version': '7.0.2.0',
     "category" : 'Bluestar/Generic module',
     'complexity': "easy",
     'description': """A list of Swiss resident permit""",

f61db82e39e52c06b91a6dc8477585658335036c
Hervé Martinet <herve.martinet@blues2.ch>
2014-04-24 16:23:40 +0200

    Add a module to manage multiple phone numbers on partner.

---
+++ bss_partner_multi_phone/__openerp__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 Bluestar Solutions Sàrl (<http://www.blues2.ch>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Partner Multiple Phone Numbers',
+    'version': 'master',
+    "category": 'Bluestar/Generic module',
+    'complexity': "easy",
+    'description': """
+Partner multiple phone numbers
+==============================
+
+With this module, the parter has a dynamic list of phone numbers, with a
+category.
+
+All existing base field (phone, mobile, fax) are synchronized with
+automatically created category in both directions.
+
+A system administrator can add custom category.
+    """,
+    'author': u'Bluestar Solutions Sàrl',
+    'website': 'http://www.blues2.ch',
+    'depends': ['bss_phonenumbers'],
+    'data': ['phone_category_data.xml',
+             'partner_view.xml', ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'images': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ bss_partner_multi_phone/phone_category.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 Bluestar Solutions Sàrl (<http://www.blues2.ch>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import osv, fields
+
+
+class communication_mode_category(osv.osv):
+
+    _name = 'bss.phone.category'
+    _description = "Phone Category"
+
+    _columns = {
+        'name': fields.char('Name', size=32, translate=True, required=True),
+        'required': fields.boolean('Required'),
+    }
+
+    _defaults = {
+        'required': False,
+    }
+
+communication_mode_category()

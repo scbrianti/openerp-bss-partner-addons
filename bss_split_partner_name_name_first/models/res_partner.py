064cbc1882af74f9fa18b57cf7dc07194339e3c2
Hervé Martinet <herve.martinet@bluestar.solutions>
2017-11-14 10:50:07 +0100

    Fix bug with name field update

--- bss_split_partner_name_name_first/models/res_partner.py
+++ bss_split_partner_name_name_first/models/res_partner.py
@@ -8,7 +8,10 @@ from odoo import models
 class Parter(models.Model):
     _inherit = 'res.partner'
 
-    def _full_name(self, first_name, last_name):
-        return '%s %s' % (last_name, first_name)
+    @staticmethod
+    def _full_name(first_name, last_name):
+        """Private method to override if you want to change the computed name.
+        """
+        return "{} {}".format(last_name, first_name)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
